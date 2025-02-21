e750b8ffc257502af93c0e4656b67abf7c06bb05
Roton02 <rph645102@gmail.com>
2025-02-21 07:16:12 +0600

    use multer and coudinary  for sending image in coudinary

--- src/app/modules/Products/Bikes.route.ts
+++ src/app/modules/Products/Bikes.route.ts
@@ -3,13 +3,15 @@ import { bikesController } from './Bikes.controller'
 import zodValidator from '../../middleware/validator'
 import BikeZodValidation from './Bike.validation'
 import auth from '../../middleware/auth'
+import upload from '../../utils/sendImageCloudinary'
 
 const BikesRouter = express.Router()
 
 BikesRouter.post(
   '/',
   auth('admin'),
-  zodValidator(BikeZodValidation),
+  // zodValidator(BikeZodValidation),
+  upload.single('file'),
   bikesController.createBike
 )
 BikesRouter.get('/', bikesController.getAllBikes)
